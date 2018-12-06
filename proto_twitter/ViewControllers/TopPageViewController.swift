08d8dee941adcbe4b0038174e8ae50f9baa0c49a
fukuda_kosuke <fukuda_kosuke@div-officenoMacBook-Pro.local>
2018-12-06 13:36:29 +0900

    横スワイプでプロフィール用の画面が出てくるように変更

--- proto_twitter/ViewControllers/TopPageViewController.swift
+++ proto_twitter/ViewControllers/TopPageViewController.swift
@@ -10,10 +10,26 @@ import UIKit
 
 class TopPageViewController: UIViewController {
 
+    @IBOutlet weak var topPageScrollView: UIScrollView!
+    
+    let contentViewController = UINavigationController(rootViewController: UIViewController())
+    let profileViewController = ProfileViewController()
+    private var isShownSidemenu: Bool {
+        return profileViewController.parent == self
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "トップページ"
 
-        // Do any additional setup after loading the view.
+        contentViewController.viewControllers[0].view.backgroundColor = .white
+        contentViewController.viewControllers[0].navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Sidemenu", style: .plain, target: self, action: #selector(sidemenuBarButtonTapped(sender:)))
+        addChildViewController(contentViewController)
+        view.addSubview(contentViewController.view)
+        contentViewController.didMove(toParentViewController: self)
+        
+        profileViewController.delegate = self
+        profileViewController.startPanGestureRecognizing()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +37,32 @@ class TopPageViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @objc private func sidemenuBarButtonTapped(sender: Any) {
+        showSidemenu(animated: true)
+    }
+    
+    private func showSidemenu(contentAvailability: Bool = true, animated: Bool) {
+        if isShownSidemenu { return }
+        
+        addChildViewController(profileViewController)
+        profileViewController.view.autoresizingMask = .flexibleHeight
+        profileViewController.view.frame = contentViewController.view.bounds
+        view.insertSubview(profileViewController.view, aboveSubview: contentViewController.view)
+        profileViewController.didMove(toParentViewController: self)
+        if contentAvailability {
+            profileViewController.showContentView(animated: animated)
+        }
+    }
+    
+    private func hideSidemenu(animated: Bool) {
+        if !isShownSidemenu { return }
+        
+        profileViewController.hideContentView(animated: animated, completion: { (_) in
+            self.profileViewController.willMove(toParentViewController: nil)
+            self.profileViewController.removeFromParentViewController()
+            self.profileViewController.view.removeFromSuperview()
+        })
+    }
 
     /*
     // MARK: - Navigation
@@ -31,5 +73,28 @@ class TopPageViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
 }
+
+extension TopPageViewController: ProfileViewControllerDelegate {
+    func parentViewControllerForProfileViewController(_ sidemenuViewController: ProfileViewController) -> UIViewController {
+        return self
+    }
+    
+    func shouldPresentForProfileViewController(_ sidemenuViewController: ProfileViewController) -> Bool {
+        /* You can specify sidemenu availability */
+        return true
+    }
+    
+    func sidemenuViewControllerDidRequestShowing(_ sidemenuViewController: ProfileViewController, contentAvailability: Bool, animated: Bool) {
+        showSidemenu(contentAvailability: contentAvailability, animated: animated)
+    }
+    
+    func sidemenuViewControllerDidRequestHiding(_ sidemenuViewController: ProfileViewController, animated: Bool) {
+        hideSidemenu(animated: animated)
+    }
+    
+    func sidemenuViewController(_ sidemenuViewController: ProfileViewController, didSelectItemAt indexPath: IndexPath) {
+        hideSidemenu(animated: true)
+    }
+}
+
