ceec094beaa20845fa2e3f9d74feccbc1fa99691
fukuda_kosuke <fukuda_kosuke@div-officenoMacBook-Pro.local>
2018-12-16 15:33:34 +0900

    キーボードが表示された際にキーボードの動きに合わせて動くViewを追加

--- proto_twitter/ViewControllers/TopPage/SendTweetViewController.swift
+++ proto_twitter/ViewControllers/TopPage/SendTweetViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class SendTweetViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var tweetTextView: UITextView!
-    @IBOutlet weak var selectPhotoButton: UIButton!
     @IBOutlet weak var tweetPhotoimageView: UIImageView!
+    
+    @IBOutlet weak var footerView: UIView!
+    @IBOutlet weak var selectPhotoButton: UIButton!
+    
+    var screenHeight:CGFloat!
+    var screenWidth :CGFloat!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +26,11 @@ class SendTweetViewController: UIViewController {
         tweetPhotoimageView.contentMode = UIViewContentMode.scaleAspectFill
         // 入力がなければreturnキーを無効にする
         tweetTextView.enablesReturnKeyAutomatically = true
+        
+        // 画面サイズ取得
+        let screenSize: CGRect = UIScreen.main.bounds
+        screenWidth  = screenSize.width
+        screenHeight = screenSize.height
         
     }
     
@@ -35,6 +46,14 @@ class SendTweetViewController: UIViewController {
                                                                  style : UIBarButtonItemStyle.plain,
                                                                  target: self,
                                                                  action:  #selector(SendTweetViewController.newTweet))
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(SendTweetViewController.keyboardWillShow(_:)),
+                                               name: NSNotification.Name.UIKeyboardWillShow,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(SendTweetViewController.keyboardWillHide(_:)) ,
+                                               name: NSNotification.Name.UIKeyboardWillHide,
+                                               object: nil)
         // テキストフィールドにフォーカスを合わせる
         tweetTextView.becomeFirstResponder()
     }
@@ -57,6 +76,65 @@ class SendTweetViewController: UIViewController {
         present(imagePickerController, animated: true)
     }
     
+    @objc func keyboardWillShow(_ notification: Notification) {
+        let info = notification.userInfo!
+        
+        let keyboardFrame = (info[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        guard let window = UIApplication.shared.keyWindow else {
+            return
+        }
+        let bottomPadding = window.safeAreaInsets.bottom
+//        let footerViewY = footerView.frame.origin.y
+//        let footerViewHeight = footerView.frame.size.height
+//        var footerViewBottom = footerViewY + footerViewHeight
+//        let viewHeight = view.bounds.size.height
+//        let difference = viewHeight - footerViewBottom - bottomPadding
+//        footerView.frame.origin.y = footerViewY + difference
+//        if let navigationController = navigationController,
+//            !navigationController.isNavigationBarHidden,
+//            let naviBarBottomPositionY = navigationController.navigationBar.subviews.first?.bounds.size.height {
+//            footerViewBottom -= naviBarBottomPositionY - difference
+//        }
+        let distance = keyboardFrame.size.height - bottomPadding
+        
+//        // window
+//        guard let window = UIApplication.shared.keyWindow else {
+//            return
+//        }
+//        let bottomPadding = window.safeAreaInsets.bottom
+//        // view
+//        let viewHeight = view.bounds.size.height
+//        // scrollView
+//        let scrollViewTop = scrollView.frame.origin.y
+//        // bottom of footerView
+//        let footerViewFrameY  = footerView.frame.origin.y
+//        let footerViewHeight  = footerView.frame.size.height
+//        let footerViewBottom  = footerViewFrameY + footerViewHeight
+//        // screenView
+//        let viewFrameHeight  = self.view.bounds.size.height
+//        // safeArea
+//        let safeAreaHeight = (viewFrameHeight - footerViewBottom) / 2
+//        // top of keyboard
+//        let topKeyboard = footerViewBottom - (keyboardFrame.size.height - safeAreaHeight)
+////        let topKeyboard = viewFrameHeight - keyboardFrame.size.height - scrollViewTop
+//        // 重なり
+//        let distance = footerViewBottom - topKeyboard
+//
+//        let difference = viewHeight - footerViewBottom - bottomPadding
+        
+        if distance >= 0 {
+            // scrollViewのコンテツを上へオフセット + 20.0(追加のオフセット)
+//            functionScrollView.contentOffset.y = distance + 20.0
+            let transform = CGAffineTransform(translationX: 0, y: -distance)
+            footerView.transform = transform
+        }
+    }
+    
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+//        functionScrollView.contentOffset.y = 0
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -69,6 +147,8 @@ class SendTweetViewController: UIViewController {
 
 }
 
+
+//MARK: - Extension
 // ImagePicker（画像取得）についてのエクステンション
 extension SendTweetViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
@@ -95,4 +175,6 @@ extension SendTweetViewController: UITextViewDelegate {
             tweetTextView.frame.size.height = size.height
         }
     }
+    
+    
 }
